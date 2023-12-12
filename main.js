e8244d4f3b8006991ca04ba8bc3b334e5a7ad7de
Pedro Nunes <pedronunesjc@gmail.com>
2023-12-12 01:44:12 -0300

    Trabalho com Three js

---
+++ main.js
@@ -0,0 +1,80 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+document.body.appendChild(renderer.domElement);
+
+// Adicione esta linha para criar um elemento de áudio no JavaScript
+const backgroundMusic = new Audio('public/Musica/musica.mp3');
+backgroundMusic.loop = true;
+backgroundMusic.play();
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(4, 5, 11);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 5;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
+
+const spotLight = new THREE.SpotLight(0xffffff, 3, 100, 0.22, 1);
+spotLight.position.set(0, 25, 0);
+spotLight.castShadow = true;
+spotLight.shadow.bias = -0.0001;
+scene.add(spotLight);
+
+// Carregar textura para o background do cubemap
+const fundoTextureLoader = new THREE.TextureLoader();
+const fundoTexture = fundoTextureLoader.load('public/Foto/teste3.jpg');
+scene.background = fundoTexture;
+
+const loader = new GLTFLoader().setPath('public/millennium_falcon/');
+loader.load('scene.gltf', (gltf) => {
+  const mesh = gltf.scene;
+
+  mesh.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+
+  mesh.position.set(0, 1.05, -1);
+  scene.add(mesh);
+
+  document.getElementById('progress-container').style.display = 'none';
+}, (xhr) => {
+  document.getElementById('progress').innerHTML = `Carregando ${Math.max(xhr.loaded / xhr.total, 1) * 100}/100`;
+});
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
